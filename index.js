1a867dd4c121731751748a5c406460f45d82ad1c
bg560581 <bennett.galgano@gmail.com>
2023-01-15 18:17:17 -0500

    Adding 404 page route

--- index.js
+++ index.js
@@ -11,6 +11,12 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
+// 404 wildcard
+
+app.get('*', (req, res) => {
+    res.status(404)("<h1>I'm sorry, you have reached a 404 :(</h1>")
+})
+
 // listen for connections
 
 app.listen(process.env.PORT, function() { 
