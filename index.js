06aae931a2e43b8d50444c6d66856d56cbf4bd38
bg560581 <bennett.galgano@gmail.com>
2023-01-21 10:39:25 -0500

    Adding route table to README

--- index.js
+++ index.js
@@ -10,6 +10,23 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
+// PLACES INDEX PAGE
+app.get('/places', (req, res) => {
+    res.send('Hello World!')
+})
+
+// CREATE NEW PLACE
+
+
+
+
+
+
+
+
+
+
+
 app.get('*', (req, res) => {
     res.status(404).send('<h1>Im sorry, you have reached a 404 :(</h1>')
 })
