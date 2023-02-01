c97fc05b51e51eed73ddaa613f077d6d9e1a22a2
bg560581 <bennett.galgano@gmail.com>
2023-02-01 17:50:17 -0500

    Delete Functionality Implemented

--- controllers/places.js
+++ controllers/places.js
@@ -1,9 +1,12 @@
 const router = require('express').Router()
 const places = require('../models/places.js')
 
+// PLACES
+
   router.get('/', (req, res) => {
     res.render('places/index', { places })
 })
+
 
 router.post('/', (req, res) => {
     if (!req.body.pic) {
@@ -20,14 +23,42 @@ router.post('/', (req, res) => {
     res.redirect('/places')
   })
 
-
-
-
+//   NEW POST
   router.get('/new', (req, res) => {
     res.render('places/new')
+})
+
+
+router.get('/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+      res.render('error404')
+    }
+    else if (!places[id]) {
+      res.render('error404')
+    }
+    else {
+      res.render('places/show', { place: places[id], id })
+    }
   })
+  
+  router.delete('/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+      res.render('error404')
+    }
+    else if (!places[id]) {
+      res.render('error404')
+    }
+    else {
+      places.splice(id, 1)    
+      res.redirect('/places')
+    }
+  })
+  
+  
+  
   
 
 
 module.exports = router
-  
